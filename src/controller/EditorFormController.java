5d78c1bab2ab00dc19741315eba66167d242a5e1
Anuradha Subasinghe <subasinghe963@gmail.com>
2021-06-26 09:06:05 +0530

    Created UI

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class EditorFormController {
+    public void mnuItemNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemExit_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemSelectAll_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFindNext_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFindPrevious_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceAll_OnAction(ActionEvent actionEvent) {
+    }
 }
